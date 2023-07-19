3f00d1987ba072a664cf5e16141e80cb7e348fe6
Md Umar <mohammadumar389@gmail.com>
2023-07-19 18:32:42 +0530

    Handling Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     // const currentYear = new Date().getFullYear();
   return (
-    <footer style={{ position: 'fixed', bottom: 0, width: '100%', textAlign: 'center' }}>
+    <footer style={{ position: 'sticky', top: '100%', width: '100%', textAlign: 'center' }}>
     <p>&copy; 2023 Password Manager. All rights reserved.</p>
   </footer>
   )
